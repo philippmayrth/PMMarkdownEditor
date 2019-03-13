799722195fe96eadc167b3b2a11481e0a0c537b7
Philipp Mayr <me@philipp-mayr.de>
2019-03-13 23:54:06 +0100

    Started porting to Windows.

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 import os
 
-os.system("$(pwd)/electron-v1.8.2-beta.4-mas-x64/Electron.app/Contents/MacOS/Electron --debug=5858 $(pwd)/Code")
+if os.system == "darwin":
+    os.system("$(pwd)/electron-v1.8.2-beta.4-mas-x64/Electron.app/Contents/MacOS/Electron --debug=5858 $(pwd)/Code")
+elif os.system == "nt":
+    raise NotImplementedError()
+elif os.system == "unix":
+    raise NotImplementedError()
+else:
+    raise Exception("Unknown OS - dont know how to run here.")
